9542f0f957a61283194bf52a2c2db015f3e77432
Fredrick Adjei Quansah <kwaadjei015@gmail.com>
2023-07-27 04:28:19 +0000

    add item

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useReducer, useRef } from "react";
 import Container from "./components/Container/Container";
 import AppHeader from "./components/AppHeader/AppHeader";
 import EnterBox from "./components/EnterBox/EnterBox";
 import Todo from "./components/Todo/Todo";
 import AppFooter from "./components/AppFooter/AppFooter";
 
+const data = [
+  {
+    item:'First item',
+    completed:false,
+    active:false,
+  }
+]
 
+const ACTIONS = {
+  add:'ADD_ITEM', 
+  search:'SEARCH_ITEM',
+  active:'ACTIVE_ITEMS',
+  completed:'COMPLETED_ITEMS', 
+  all:'ALL_ITEMS'
+}
+
+const reducerFn=(state, action)=>{
+if(action.type === ACTIONS.add){
+  const newTodo = {
+    item:action.payload,
+    completed:false,
+    active:true
+  }
+  return [...state, newTodo];
+}
+return state
+}
 
 function App() {
+  const inputRef = useRef();
+  const [todoState, dispatchTodo] = useReducer(reducerFn, data)
+
+  const onAddItemHandler=()=>{
+    dispatchTodo({type:ACTIONS.add, payload:inputRef.current.value})
+    //forced dom manipulation
+    inputRef.current.value='';
+  }
+
+
+
+  //list of items
+  const listOfItems = todoState.map((item, index)=>{
+    return (
+        <Todo key={index} todo={item.item}/>
+    )
+  })
+
   return (
     <>
     <Container>
       <AppHeader title="Things to do"/>
-      <EnterBox/>
-      <Todo todo="My first Todo"/>
-      <AppFooter/>
+      <EnterBox ref={inputRef}/>
+      {listOfItems}
+      <AppFooter addItemHandler={onAddItemHandler}/>
     </Container>
     </>
   )
--- src/components/AppFooter/AppFooter.jsx
+++ src/components/AppFooter/AppFooter.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from './AppFooter.module.css';
+import {FaPlus} from 'react-icons/fa';
+import { FaSearch } from "react-icons/fa";
 
 
-function AppFooter(){
+function AppFooter(props){
     return (
       <>
         <div className={styles.appfooter__container}>
           <div className={styles.appfooter__leftside}>
             <div className={styles.appfooter__leftside__buttons}>
-              <button>Add</button>
-              <button>Search</button>
+              <FaPlus size={23} onClick={props.addItemHandler}/>
+              <FaSearch size={23} onClick={props.searchItemHandler}/>
             </div>
             <div className={styles.divider}></div>
             <div className={styles.appfooter__left__side__items}>
@@ -17,9 +19,9 @@ function AppFooter(){
             </div>
           </div>
           <div className={styles.appfooter__rightside__buttons}>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
+            <button onClick={props.findAllItemsHandler}>All</button>
+            <button onClick={props.findActiveItemsHandler}>Active</button>
+            <button onClick={props.findCompletedItemsHandler}>Completed</button>
           </div>
         </div>
       </>
